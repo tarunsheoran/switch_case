3b7884f4c39713ad3836039b38c86d572e657bb1
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-10 22:09:24 +0530

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,24 @@
+/*#include <iostream>
+using namespace std;
+
+	void greet(){
+		cout<<"Namastey Duniya";
+	}
+	int main(){
+		greet();
+	}*/
+	
+	
+#include <iostream>
+using namespace std;
+void display(int a,float b){
+	cout<<"The first number : "<<a<<endl;
+	cout<<"The second number : "<<b;	
+}
+int main(){
+	int a=2;
+	double b=5.4;
+	
+	display(a,b);
+}
+
